7c7cb91d933f03d64cf7c75e720139e96485e9f1
jin pettit <jinpettit@gmail.com>
2022-08-28 03:18:03 -0700

    fix

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -13,7 +13,7 @@ const Playlist = () => {
     const [tracks, setTracks] = useState(null);
     const [audioFeatures, setAudioFeatures] = useState(null);
     const [sortValue, setSortValue] = useState('');
-    const sortOptions = ['danceability'];
+    const sortOptions = ['danceability', 'tempo', 'energy'];
 
     useEffect(() => {
         const fetchData = async () => {
